adf8ea29585b753ebf1e2f93a265bcec4543891f
KAUE HLATKI SPACKI <kaue.spacki@cs.up.edu.br>
2025-04-04 11:03:28 -0300

    Melhorado a página de login

--- login.php
+++ login.php
@@ -6,44 +6,28 @@
     <title>Zoológico Terra Selvagem - Login</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<style>
-    form {
-        padding: 10px;
-    }
-    .conteudo {
-        display: flex;
-        align-items: left;
-        justify-content: left;
-        text-align: left;
-    }
-  
-</style>
 <body>
     <div class="main">
         <?php include("cabecalho.html") ?>
         <div class="conteudo">
-            <h1 style="font-size: 25px">Login</h1>
-            <form method="post">
+            <h1>Login</h1>
+            <form style="height: 200px" method="post">
                 Usuário<br> <input name="usuario" type="text"><br>
                 Senha<br> <input name="senha" type="password" required><br>
-                <br>
-                <input type="submit" name="logar" value="Entrar">
+                <input style="margin: 10px" type="submit" name="logar" value="Entrar">
 
                 <?php 
                 if(isset($_POST['usuario']) && isset($_POST['senha'])){
                     if(($_POST['usuario'] == "admin") && ($_POST['senha'] == "123")){
                         header("location: http://localhost/zoologico-terra-selvagem/admin.php");
                     }else{
-                        echo"<br>Senha inválida";
+                        echo"<p style='color: red;'><br>Senha inválida</p>";
                     }
-                    
                 }
-
-                
                 ?>
+
             </form>
         </div>
     </div>
 </body>
 </html>
-
